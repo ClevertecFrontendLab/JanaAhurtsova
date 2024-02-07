276c90d24eee726ccf7cb3854e27bf97c1356d62
Jana Ahurtsova <byjankaaa@gmail.com>
2024-02-07 14:19:45 +0300

    feat: add additional packages

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import path from 'path';
 
 import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-    plugins: [react()],
+    plugins: [react(), tsconfigPaths()],
     server: {
         host: true,
         port: 3000,
